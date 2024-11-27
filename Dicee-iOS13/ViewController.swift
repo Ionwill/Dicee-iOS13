67b11a660450e8978b34ec7833f0401e7d467f9f
Ayan Williams <101142116+Ionwill@users.noreply.github.com>
2024-11-27 16:05:20 -0600

    push

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -4,17 +4,34 @@
 //
 //  Created by Angela Yu on 11/06/2019.
 //  Copyright © 2019 London App Brewery. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+        //diceImageView1.image
+        //diceImageView1.alpha
+        // #imageLiteral(
+        
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray.randomElement()
+        
+        diceImageView2.image = diceArray.randomElement()
+        
+        
+        
+        
     }
-
-
+    
+    
 }
 
